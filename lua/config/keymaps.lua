d7181fe2bdb5b2fa38e8d6c2c04e0d4d9079a82f
bibinbiju <bibinbiju482@gmail.com>
2026-08-21 11:42:41 +0530

    telescope keympas to keympas.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,26 @@
 
 vim.keymap.set("n", "<leader>ha", ":Alpha<CR>", { desc = "Open Dashboard" })
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
+
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>ff", function()
+  LazyVim.pick("files", { root = false })()
+end, { desc = "Find Files (cwd)" })
+
+vim.keymap.set("n", "<leader>fF", function()
+  LazyVim.pick("files")()
+end, { desc = "Find Files (Root Dir)" })
+
+vim.keymap.set("n", "<leader>sg", function()
+  LazyVim.pick("live_grep", { root = false })()
+end, { desc = "Grep (cwd)" })
+
+vim.keymap.set("n", "<leader>sG", function()
+  LazyVim.pick("live_grep")()
+end, { desc = "Grep (Root Dir)" })
+
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {
+  desc = "Find Help",
+})
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,47 +2,48 @@ return {
   "nvim-telescope/telescope.nvim",
   opts = {
     defaults = {
-      layout_strategy = "horizontal",
+      layout_strategy = "vertical",
       layout_config = {
         vertical = {
           width = 0.9,
           height = 0.9,
           preview_height = 0.65,
+          mirror = true,
         },
       },
     },
 
     pickers = {
       find_files = {
-        layout_strategy = "horizontal",
+        layout_strategy = "vertical",
       },
       live_grep = {
-        layout_strategy = "horizontal",
+        layout_strategy = "vertical",
       },
     },
   },
-  keys = {
-    {
-      "<leader>ff",
-      LazyVim.pick("files", { root = false }),
-      desc = "Find Files (cwd)",
-    },
-
-    {
-      "<leader>fF",
-      LazyVim.pick("files"),
-      desc = "Find Files (Root Dir)",
-    },
-    {
-      "<leader>sg",
-      LazyVim.pick("live_grep", { root = false }),
-      desc = "Grep (cwd)",
-    },
-
-    {
-      "<leader>sG",
-      LazyVim.pick("live_grep"),
-      desc = "Grep (Root Dir)",
-    },
-  },
+  -- keys = {
+  --   {
+  --     "<leader>ff",
+  --     LazyVim.pick("files", { root = false }),
+  --     desc = "Find Files (cwd)",
+  --   },
+  --
+  --   {
+  --     "<leader>fF",
+  --     LazyVim.pick("files"),
+  --     desc = "Find Files (Root Dir)",
+  --   },
+  --   {
+  --     "<leader>sg",
+  --     LazyVim.pick("live_grep", { root = false }),
+  --     desc = "Grep (cwd)",
+  --   },
+  --
+  --   {
+  --     "<leader>sG",
+  --     LazyVim.pick("live_grep"),
+  --     desc = "Grep (Root Dir)",
+  --   },
+  -- },
 }
